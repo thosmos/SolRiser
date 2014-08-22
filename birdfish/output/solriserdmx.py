631a2dd0e47d0e0e5293d58ce2fcaa54cf10c812
Thomas Spellman <thos37@gmail.com>
2014-08-22 10:16:37 -0700

    sort of works on the yun

--- birdfish/output/solriserdmx.py
+++ birdfish/output/solriserdmx.py
@@ -7,16 +7,13 @@ class SolRiserDmx(BaseNetwork):
         super(SolRiserDmx,self).__init__()
         self.port = serial_port
         self.serial = serial.Serial(serial_port, 115200, timeout=0)
-        #ser = serial.Serial("/dev/tty.usbmodemfa131", 57600, timeout=1)
 
     def send_data(self):
-        # print "writing data to Arduino"
+        
         self.update_data()
-        print self.data
-        #try:
-        self.serial.write(self.data);
-        #except serial.SerialException, (value,message):
-        #    print "Serial Error, continuing: %s" % message
-        #    self.wrapper = ClientWrapper()
-        #    self.client = self.wrapper.Client()
-        #    time.sleep(.5)
+
+        #print self.data
+
+        # use a hack to convert the binary array to a string to satisfy the Yun's pyserial implementation
+        self.serial.write("".join(map(chr, self.data)));
+        
